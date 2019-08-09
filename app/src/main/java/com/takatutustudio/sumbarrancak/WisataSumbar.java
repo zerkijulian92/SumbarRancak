d486fd652310c530d95a15192bd483ade974fb6d
zerkijulian92 <zerkijuliandri92@gmail.com>
2019-08-09 11:12:32 +0700

    Membuat class WisataSumbar.java dan getter and setter

---
+++ app/src/main/java/com/takatutustudio/sumbarrancak/WisataSumbar.java
@@ -0,0 +1,38 @@
+package com.takatutustudio.sumbarrancak;
+
+public class WisataSumbar {
+
+    private String name, remarks, photo, deskripsi;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+
+    public void setDeskripsi(String deskripsi) {
+        this.deskripsi = deskripsi;
+    }
+}
