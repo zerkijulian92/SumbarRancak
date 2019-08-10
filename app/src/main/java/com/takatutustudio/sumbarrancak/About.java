2aa553bd2c838b1a199109faca13d7e1b4498923
zerkijulian92 <zerkijuliandri92@gmail.com>
2019-08-10 14:38:05 +0700

    Perbaikan Tampilan About

--- app/src/main/java/com/takatutustudio/sumbarrancak/About.java
+++ app/src/main/java/com/takatutustudio/sumbarrancak/About.java
@@ -2,12 +2,26 @@ package com.takatutustudio.sumbarrancak;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class About extends AppCompatActivity {
+
+    Button btn_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        btn_back    = findViewById(R.id.btn_back);
+
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
     }
 }
