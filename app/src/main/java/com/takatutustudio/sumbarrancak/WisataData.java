6599639994350b49bf9ec2797521f70a7e27afc2
zerkijulian92 <zerkijuliandri92@gmail.com>
2019-08-10 15:42:35 +0700

    Selesai memasukan data wisata

--- app/src/main/java/com/takatutustudio/sumbarrancak/WisataData.java
+++ app/src/main/java/com/takatutustudio/sumbarrancak/WisataData.java
@@ -6,23 +6,74 @@ public class WisataData {
     public static String[][] data = new String[][]{
             {
                 "Puncak Chinangkiak",
-                    "Jalan singkarak solok",
-                    "http://visitsolok.com/files/2017-11/11111111111.jpg",
-                    "Deskripsi lorem ipsum"
+                    "Singkarak, X Koto Singkarak, Solok, Sumatera Barat ",
+                    "https://i1.wp.com/penginapan.net/wp-content/uploads/Bukik-Cinangkiak.jpg?resize=460%2C300&ssl=1",
+                    "Bukit Cinangkiak (Bukik Chi Nang Kiek) adalah tempat wisata yang terletak di Kecamatan X Koto Singkarak, Kabupaten Solok, Sumatera Barat. Destinasi wisata anyar ini banyak diperbincangkan di media sosial karena menawarkan panorama yang apik, dipadukan dengan sejumlah wahana serta fasilitas pendukung yang memadai."
             },
 
             {
-                "Danau Maninjau",
-                    "Jalan Maninjau",
-                    "https://m2indonesia.com/wp-content/uploads/sites/2/2015/07/maninjau-lake.jpg",
-                    "Deskripsi lorem ipsum"
+                "Puncak Lawang",
+                    "Lawang, Matur, Kabupaten Agam, Sumatera Barat",
+                    "https://upload.wikimedia.org/wikipedia/id/thumb/2/21/Paralayang_di_puncak-lawang.jpg/220px-Paralayang_di_puncak-lawang.jpg",
+                    "Puncak Lawang merupakan nama suatu puncak dataran tinggi di Kabupaten Agam Sumatra Barat. Dari tempat ini, kita bisa meihat birunya Danau Maninjau. Puncak Lawang terletak di Kecamatan Matur, Kabupaten Agam, Sumatra Barat. Ini daerah puncak menuju Danau Maninjau. Dari sini kita dapat melihat pemandangan Danau Maninjau secara utuh"
             },
 
             {
-                "Danau Singkarak",
-                    "Jalan singkarak",
-                    "https://media.beritagar.id/2015-10/0d8e6c4b783aec3702ecd494f7149011.jpg",
-                    "Deskripsi lorem ipsum"
+                    "Puncak Cemara",
+                    "Saringan, Barangin, Kota Sawah Lunto, Sumatera Barat ",
+                    "https://cdn.idntimes.com/content-images/community/2019/07/puncak-cemara-sawahlunto-2ceb933c8678b147c22b119e794ae2bd.jpeg",
+                    "Objek Wisata Puncak Cemara Terletak tidak jauh dari pusat Kota Sawahlunto membuat objek wisata keluarga ini cocok untuk anda yang membutuhkan udara serta pemandangan yang indah. Berjarak sekitar 2-3 jam perjalanan dari pusat Ibukota Provinsi Sumatera Barat (Padang) dan untuk mengetahui lokasi Puncak Cemara ini pun tidak lah terlalu sulit, karena ketika kita akan memasuki kota Sawahlunto dari kejauhan lokasinya sudah tampak walaupun sedikit. "
+            },
+
+            {
+                    "Puncak Aua Sarumpun",
+                    "Iii Koto, Rambatan, Kabupaten Tanah Datar, Sumatera Barat",
+                    "https://cdn.idntimes.com/content-images/community/2019/07/www-sumbar-tavel-dyxwt-2-346aee992623ca70be2e69c97b38645c.jpg",
+                    "Aua Sarumpun adalah nama dari sebuah bukit yang berlokasi di Jorong Siturah, Nagari III Koto, Kecamatan Rambatan, Kabupaten Tanah Datar. Bukit ini dinamakan Aua Sarumpun sesuai dengan keberadaan serumpun aua (aur / bambu) di puncak bukit. Lokasi ini sebenarnya bukanlah objek wisata dadakan atau baru. Keberadaannya sudah lama diketahui oleh para pecinta alam, peminat olahraga ekstrem dan para penggemar buru babi."
+            },
+
+            {
+                    "Puncak Langkisau",
+                    "Salido, Iv Jurai, Kabupaten Pesisir Selatan, Sumatera Barat",
+                    "https://cdn.idntimes.com/content-images/community/2019/07/53414aac8e219-1bb23ba7b847fa1b6a2df1a74ffd9a99.jpg",
+                    "Pemandangan Bukit Langkisau yang menakjubkan telah menjadi tujuan alternatif setelah Pantai Carocok yang terkenal di Kabupaten Pesisir Selatan. Keindahan alam di puncak Bukit Langkisau bahkan akan terlihat lebih indah saat matahari terbenam dan matahari terbit. Puncak bukit memiliki ketinggian sekitar 400 meter. Menuju puncak Langkisau Anda bisa menggunakan kendaraan, karena jalannya sudah diaspal. Tentunya lebih mudah, wisatawan datang ke puncak Bukit Langkisau. Sesampainya di area parkir, Anda masih harus berjalan kaki sekitar 25 meter menaiki tangga menuju puncak bukit."
+            },
+
+            {
+                    "Puncak Gobah",
+                    "Singkarak, X Koto Singkarak, Solok, Sumatera Barat ",
+                    "https://cdn.idntimes.com/content-images/community/2019/07/20429620-1927015234213726-5714342268524739160-n-0c215453196a9bdb4065c3acaf4ce2e8.jpg",
+                    "Inilah satu objek wisata di Kabupaten Solok yang menyajikan pemandangan danau singkarak dari ketinggian. Puncak Gobah. Destinasi wisata yang satu ini bisa dibilang masih baru, namun cukup populer terutama di kalangan wisatawan. Puncak Gobah terletak di Nagari Aripan, Kecamatan X Koto Singkarak, Kabupaten Solok, Sumatera Barat. Jika dari kota Solok menuju lokasi jarak yang ditempuh kurang lebih sekitar 20 k"
+            },
+
+            {
+                    "Puncak Gagoan",
+                    "Paninggahan, Junjung Sirih, Solok, Sumatera Barat ",
+                    "https://cdn.idntimes.com/content-images/community/2019/07/43240585-147304526227250-5611751693107477259-n-13899794e1c9e6bf456ad7dae1bca372.jpg",
+                    "Bagi kamu yang ingin menikmati wisata alam sekaligus menguji adrenalin maka Puncak Gagoan adalah tempat yang pas. Puncak ini dihiasi dengan ilalang dan tebing bebatuan yang kemiringannya terbilang curam. Jalurnya cukup ekstrim karena kamu harus mendaki dan menurun untuk sampai ke Puncak ini."
+            },
+
+            {
+                    "Puncak Tonang",
+                    "Sundata, Lubuk Sikaping, Kabupaten Pasaman, Sumatera Barat",
+                    "https://paketwisatapadang.com/wp-content/uploads/2019/01/suasana-romantis-di-puncak-tonang.jpg",
+                    "Taman Bunga Puncak Tonang tentu tidak hadir dengan tiba-tiba. Ada tangan-tangan kreatif di belakangnya. Mereka adalah orang-orang yang menyulap sebuah lahan tidur berukuran 20 hektar menjadi kebun bunga cantik. Ya. Semula, Taman Bunga Puncak Tonang adalah lahan kosong yang tidak difungsikan untuk apa pun. Penggarapan lahan tidur ini sebagai destinasi wisata, dilakukan sejak beberapa tahun lalu."
+            },
+
+            {
+                    "Puncak Ampangan",
+                    "Aua Kuniang, Payakumbuh Selatan, Ampangan, Payakumbuh",
+                    "https://www.indonesiakaya.com/uploads/_images_gallery/4__IMG_2299_Panorama_perbukitan_di_sekitar_kota_Payakumbuh_dilihat_dari_puncak_Ampangan.jpg",
+                    "Daerah ini merupakan salah satu sentra botuang (bambu), karena postur daerahnya yang berlembah dan lereng bukit. Selain sentra Botuang, daerah yang berhawa dingin berhembus sepoi-sepoi ini ternyata juga sentra ubi kayu dan produksi pertanian lainnya. Selain warganya yang ramah, di daerah ini juga masih banyk kita jumpai tanaman aren."
+            },
+
+
+
+            {
+                "Puncak Mandeh",
+                    "Ampang Pulai, Koto XI Tarusan, Kabupaten Pesisir Selatan, Sumatera Barat",
+                    "https://cdn.idntimes.com/content-images/community/2019/07/1557541-174995096219021-4202267565932110482-n-e56a40febe3f5c0dc6266c6e2d6cc367.jpg",
+                    "Puncak mandeh adalah tempat wisata yang menyerupai Raja Ampat di Kabupaten Pesisir Selatan dan berbatasan langsung dengan kota Padang. Puncak Mandeh terletak kurang lebih 56km dari Kota Padang dan dapat ditempuh menggunakan mobil selama 1 jam perjalanan. Kawasan wisata Mandeh sendiri mempunyai area seluas 18.000 Ha dan meliputi 7 desa dari 3 nagari yang penduduknya bekerja sebagai nelayan, beternak dan bertani."
             }
 
     };
