715d993286b67ab771c328a2f19e72060916bdc8
zerkijulian92 <zerkijuliandri92@gmail.com>
2019-08-12 13:52:27 +0700

    Aplikasi FIX | Menambahkan animasi pada splash screen dan button logout ( Profil )

--- app/src/main/java/com/takatutustudio/sumbarrancak/About.java
+++ app/src/main/java/com/takatutustudio/sumbarrancak/About.java
@@ -3,9 +3,13 @@ package com.takatutustudio.sumbarrancak;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 
 public class About extends AppCompatActivity {
+    //Deklarasi
+    Animation btt;
 
     Button btn_back;
 
@@ -14,14 +18,22 @@ public class About extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
+        //load animasi
+        btt = AnimationUtils.loadAnimation(this, R.anim.btt);
+
+        //Load element
+        btn_back    = findViewById(R.id.btn_back);
+
+        //Run Animasi
+        btn_back.startAnimation(btt);
+
         //Merubah judul halaman Detail Wisata
         if (getSupportActionBar() != null) {
             getSupportActionBar().setTitle("Copyright");
         }
         //----
 
-        btn_back    = findViewById(R.id.btn_back);
-
+        //Kembali
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/takatutustudio/sumbarrancak/Splash.java
+++ app/src/main/java/com/takatutustudio/sumbarrancak/Splash.java
@@ -4,13 +4,33 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Splash extends AppCompatActivity {
+    //deklarasi
+    Animation app_splash, btt;
+    ImageView app_logo;
+    TextView app_subtitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //load animasi
+        app_splash = AnimationUtils.loadAnimation(this, R.anim.app_splash);
+        btt = AnimationUtils.loadAnimation(this, R.anim.btt);
+
+        //Load element
+        app_logo       = findViewById(R.id.app_logo);
+        app_subtitle   = findViewById(R.id.app_subtitle);
+
+        //Run Animasi
+        app_logo.startAnimation(app_splash);
+        app_subtitle.startAnimation(btt);
 
         //Setting Timer Untuk 1 Detik
         Handler handler = new Handler();
@@ -22,6 +42,6 @@ public class Splash extends AppCompatActivity {
                 startActivity(gotomain);
                 finish();
             }
-        }, 2200); //1000 ms = 1second
+        }, 2400); //1000 ms = 1second
     }
 }
